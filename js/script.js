f55c6e0e4432bfe1753e0c7a8b515789804ecfdc
YuriyShandor <yuriyshandor@gmail.com>
2017-08-19 19:06:01 +0300

    Add Section Banner

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
-  $('#search-icon').on('click', function() {
-    $('.top-menu__input-search').toggleClass('top-menu__input-search_click');
+  $('.search-icon').on('click', function() {
+    $('.top-menu__input-search').addClass('top-menu__input-search_click');
+    $('.search-icon').addClass('search-icon_click');
+    $('.close-search-icon').addClass('close-search-icon_click');
+  });
+
+  $('.close-search-icon').on('click', function() {
+    $('.top-menu__input-search').removeClass('top-menu__input-search_click');
+    $('.search-icon').removeClass('search-icon_click');
+    $('.close-search-icon').removeClass('close-search-icon_click');
+  });
+
+  $('.banner-slider').slick({
+   slidesToShow: 1,
+   slidesToScroll: 1,
+   nextArrow: $(".slider-nav_right"),
+   prevArrow: $(".slider-nav_left"),
+   autoplay: true,
+   autoplaySpeed: 5000,
   });
 });
